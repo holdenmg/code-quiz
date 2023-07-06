82050bfa0d65095ef703832cad8f99fabea3da8c
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-07-06 18:07:47 -0400

    Made file directories. Begun to psuedocode logic/question array

---
+++ scripts/logic.js
@@ -0,0 +1,37 @@
+
+
+ //write response and time remaining value to highscortes
+function startQuiz(){
+//on press of start, start timer and call nextQuestion to display first question
+
+
+
+}
+
+
+function nextQuestion(){
+    //pull next question from questions array
+
+}
+
+
+function correct(){
+    //call next question
+
+}
+
+
+function wrong(){
+// deduct time from timer
+//call next question
+
+}
+
+function gameOver(){
+
+    //stop the question function log score - read interval value and request initisald from user
+
+ //write response and time remaining value to highscortes
+
+ // ask if you want to play again
+}
---
+++ scripts/questions.js
@@ -0,0 +1,8 @@
+// build array of question objects
+
+//q: string
+//a1:
+//a2:
+//a3:
+//a4:
+//CA:
