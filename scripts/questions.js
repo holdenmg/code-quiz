a2f3e2aba17709173213f406530afc4766f25467
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-07-06 19:57:03 -0400

    Added 3 new questions in questions.js and built array to hold question objects

--- scripts/questions.js
+++ scripts/questions.js
@@ -6,3 +6,33 @@
 //a3:
 //a4:
 //CA:
+
+var question1 = {
+    question: "To out put a message to the web console you would use what method? " ;
+    a1:"print.out()",
+    a2:"console.log()",
+    a3:"output()",
+    a4:"log.out()",
+    correctAnswer: a2
+};
+
+
+var question2 = {
+    question: "Which of these is not a data type in javaScript? " ;
+    a1:"boolean",
+    a2:"number",
+    a3:"string",
+    a4:"pointer",
+    correctAnswer: a4
+};
+
+var question3 = {
+    question: "Which of these correctly builds an empty array in javaScript?" ;
+    a1:"newArray=()",
+    a2:"newArray={}",
+    a3:"newArray=<>",
+    a4:"newArray=[]",
+    correctAnswer: a4
+};
+
+const questions = [question1, question2, question3];
