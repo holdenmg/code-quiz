9d2b7b1a70c49012491a45e74fb1c7038c0fbcd5
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-07-06 19:05:31 -0400

    Built out basic html for index and high scores pages. No styling applied yet

--- scripts/logic.js
+++ scripts/logic.js
@@ -2,13 +2,33 @@
 
  //write response and time remaining value to highscortes
 function startQuiz(){
-//on press of start, start timer and call nextQuestion to display first question
+//on press of start, call start timer and call nextQuestion to display first question
 
 
 
 }
 
-
+function startTimer() {
+    // Sets timer
+  timer = setInterval(function() {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      if (isWin && timerCount > 0) {
+        // Clears interval and stops timer
+        clearInterval(timer);
+        winGame();
+      }
+    }
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      loseGame();
+    }
+  }, 1000);
+}
 function nextQuestion(){
     //pull next question from questions array
 
