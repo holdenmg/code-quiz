cd21e5b59e20757d32105bd0264656f0f5d8868f
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-07-07 11:36:28 -0400

    Added more questions

--- scripts/questions.js
+++ scripts/questions.js
@@ -36,30 +36,65 @@ var question3 = {
 };
 
 var question4 = {
-    question: "Which of these correctly builds an empty array in javaScript?" ,
-    a1:"newArray=()",
-    a2:"newArray={}",
-    a3:"newArray=<>",
-    a4:"newArray=[]",
-    correctAnswer: "a4"
+    question: "What does the '%' do in JavaScript?" ,
+    a1:"Multiply",
+    a2:"Returns remainder",
+    a3:"Creates a new object",
+    a4:"Points to the head",
+    correctAnswer: "a2"
 };
 
 var question5 = {
-    question: "Which of these correctly builds an empty array in javaScript?" ,
-    a1:"newArray=()",
-    a2:"newArray={}",
-    a3:"newArray=<>",
-    a4:"newArray=[]",
-    correctAnswer: "a4"
+    question: "What is the proper load order for css.reset and css.style?" ,
+    a1:"style first",
+    a2:"Order doesn't matter",
+    a3:"Reset first",
+    a4:"Reset doesn't get linked",
+    correctAnswer: "a3"
 };
 
 var question6 = {
-    question: "Which of these correctly builds an empty array in javaScript?" ,
-    a1:"newArray=()",
-    a2:"newArray={}",
-    a3:"newArray=<>",
-    a4:"newArray=[]",
-    correctAnswer: "a4"
+    question: "WHich of the following is NOT a functionality available to JavaScript?" ,
+    a1:"Multi-threading",
+    a2:"Object-oriented programming",
+    a3:"Storing data in variables",
+    a4:"Storing objects in arrays",
+    correctAnswer: "a1"
 };
 
+var question7 = {
+    question: "Which is the proper syntax for adding a list item to HTML?" ,
+    a1:"<list>",
+    a2:"<li>",
+    a3:"list-item",
+    a4:"<list-item>",
+    correctAnswer: "a2"
+};
+
+var question8 = {
+    question: "Which of these would be largest on the screen (assuming no styling)?" ,
+    a1:"<h1>",
+    a2:"<h2>",
+    a3:"<h3>",
+    a4:"<h4>",
+    correctAnswer: "a1"
+};
+
+var question9 = {
+    question: "Which of these correctly demonstrates proper variable naming conventions in JavaScript?" ,
+    a1:"newArray",
+    a2:"newarray",
+    a3:"Newarray",
+    a4:"nEwArrAy",
+    correctAnswer: "a1"
+};
+
+var question10 = {
+    question: "Which of these demonstrates proper syntax for a 'for' loop?" ,
+    a1:"for{let i= 1 , i <= 100; i++}{//some code here//}",
+    a2:"for (let i = 1; i <= 100; i++){//some code here//}",
+    a3:"for (let i = 1, i <= 100, i++){//some code here//}",
+    a4:"for (let i = 1; i <= 100; i++)(//some code here//)",
+    correctAnswer: "a2"
+};
 var questions = [question1, question2, question3, question4, question5, question6];
