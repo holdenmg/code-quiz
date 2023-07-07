fc27025fd1cf1fe45039634d5098341b0a796e69
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-07-07 18:30:47 -0400

    Added another js file titled highscore to handle gameOver function and highscore managment. Updated html script links

---
+++ scripts/highscore.js
@@ -0,0 +1,17 @@
+var gameOverMessage = document.querySelector(".gameOverMessage")
+var highScores = [];
+
+function gameOver(){
+
+    //stop the question function log score - read interval value and request initisald from user
+    window.location.href = "highscores.html";
+    gameOverMessage.textContent = "That's all!";
+   // gameOverMessage = document.createElement("h4");
+    //quizQuestion.appendChild(gameOverMessage);
+    //gameOverMessage.textContent = "Your score is: " + timerCount;
+    //clearInterval(timer);
+
+ //write response and time remaining value to highscortes
+    
+ // ask if you want to play again
+}
--- scripts/logic.js
+++ scripts/logic.js
@@ -2,22 +2,26 @@ var quizQuestion= document.querySelector(".quiz-question");
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
 var quizAnswer = document.querySelector(".quiz-answers");
+var title = document.querySelector(".title");
+var subtitle = document.querySelector(".subtitle");
+var message = document.querySelector(".message");
 var timer;
 var timerCount;
 var i = 0;
-var highScores = [];
+
 
  //write response and time remaining value to highscortes
 function startQuiz(){
 //on press of start, call start timer and call getQuestion to display first question
-
+startButton.style.display = "none"
+title.style.display = "none"
+subtitle.style.display = "none"
   timerCount = 60;
   // Prevents start button from being clicked when round is in progress
   startButton.disabled = true;
   getQuestion()
   startTimer()
 }
-
 
 function startTimer(){
     // Sets timer
@@ -32,29 +36,25 @@ function startTimer(){
     }
   }, 1000);
 }
+
 function getQuestion(){
     //pull next question from questions array and then check if user choice is correct
 
-    document.querySelector(".quiz-question").innerHTML = questions[i].question 
-    document.querySelector(".a1").innerHTML ="1. " + questions[i].a1
-    document.querySelector(".a2").innerHTML = "2. " + questions[i].a2
-    document.querySelector(".a3").innerHTML = "3. " + questions[i].a3
-    document.querySelector(".a4").innerHTML = "4. " + questions[i].a4
-    console.log("xxxxxxxxxxxxxxxxxxxxxxx");
-    
-    
+  document.querySelector(".quiz-question").innerHTML = questions[i].question 
+  document.querySelector(".a1").innerHTML ="1. " + questions[i].a1
+  document.querySelector(".a2").innerHTML = "2. " + questions[i].a2
+  document.querySelector(".a3").innerHTML = "3. " + questions[i].a3
+  document.querySelector(".a4").innerHTML = "4. " + questions[i].a4  
 }
-
-
 
 function checkCorrect(){
     // if choice matches correct answer display correct then call next question
-    quizAnswer.addEventListener("click", function(event) {
+  quizAnswer.addEventListener("click", function(event) {
       var userChoice = event.target;
       userAnswer = userChoice.getAttribute("class")
       console.log(userAnswer);
       if (questions[i].correctAnswer === userAnswer) {
-          quizQuestion.textContent = "CORRECT!"
+          message.textContent = "CORRECT!"
       }
       else{
           wrong()
@@ -66,34 +66,18 @@ function checkCorrect(){
       else{
         getQuestion();
       }
-      }); 
-    }
+  }); 
+}
   
-   
-    
-
-
 function wrong(){
 // deduct time from timer
-timerCount = timerCount -  10;
-quizQuestion.textContent = "WRONG!";
-
-
+  if(timerCount <= 10){
+    timerCount = 0
+  }
+  else{
+  timerCount = timerCount -  10;
+  message.textContent = "WRONG!";
+  }
 }
-
-function gameOver(){
-
-    //stop the question function log score - read interval value and request initisald from user
-    quizQuestion.textContent = "That's all!";
-    gameOverMessage = document.createElement("h4");
-    quizQuestion.appendChild(gameOverMessage);
-    gameOverMessage.textContent = "Your score is: " + timerCount;
-    clearInterval(timer);
-
- //write response and time remaining value to highscortes
-    
- // ask if you want to play again
-}
-
 startButton.addEventListener("click", startQuiz);
 checkCorrect();
